6b3c77fd69268c9d38761303ee6019f334bd5319
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2021-11-20 18:43:08 -0400

    Create methods select all, select, insert, update, soft delete.

--- src/doctors/doctors.service.ts
+++ src/doctors/doctors.service.ts
@@ -1,26 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateDoctorDto } from './dto/create-doctor.dto';
 import { UpdateDoctorDto } from './dto/update-doctor.dto';
+import { Doctor } from './entities/doctor.entity';
+import { Repository } from 'typeorm';
+import axios from 'axios';
 
 @Injectable()
 export class DoctorsService {
-  create(createDoctorDto: CreateDoctorDto) {
-    return 'This action adds a new doctor';
+  constructor(
+    @InjectRepository(Doctor)
+    private readonly doctorRespository: Repository<Doctor>,
+  ) {}
+  async insert({
+    name,
+    crm,
+    cep,
+    landline,
+    cellphone,
+    medicalspecialties,
+  }: CreateDoctorDto): Promise<Doctor> {
+    const doctor = this.doctorRespository.create();
+    doctor.name = name;
+    doctor.crm = crm;
+    doctor.landline = landline;
+    doctor.cellphone = cellphone;
+    doctor.CEP = (
+      await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+    ).data;
+    doctor.medicalspecialties = medicalspecialties;
+
+    return this.doctorRespository.save(doctor);
   }
 
-  findAll() {
-    return `This action returns all doctors`;
+  selectAll(): Promise<Doctor[]> {
+    return this.doctorRespository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} doctor`;
+  async select(id: string): Promise<Doctor> {
+    const doctor = await this.doctorRespository.findOne(id);
+    if (!doctor) {
+      throw new NotFoundException(`This action select a ${id} doctor`);
+    }
+    return doctor;
   }
 
-  update(id: number, updateDoctorDto: UpdateDoctorDto) {
-    return `This action updates a #${id} doctor`;
+  async update(id: string, updateDoctorDto: UpdateDoctorDto): Promise<Doctor> {
+    const doctor = await this.doctorRespository.preload({
+      id: String(id),
+      ...updateDoctorDto,
+    });
+    if (!doctor) {
+      throw new NotFoundException(`This action update a ${id} doctor`);
+    }
+    return this.doctorRespository.save(doctor);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} doctor`;
+  async softDelete(id: string): Promise<Doctor> {
+    const doctor = await this.doctorRespository.findOne(id);
+    if (!doctor) {
+      throw new NotFoundException(`This action update a ${id} doctor`);
+    }
+    return this.doctorRespository.remove(doctor);
   }
 }
